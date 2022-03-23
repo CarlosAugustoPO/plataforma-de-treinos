d4d2ad72e65499e2429df7069e8b40452e143ef3
CarlosAugustoPO <eu.carlos.aug@gmail.com>
2022-03-23 01:40:54 -0300

    refactor: detruturing props

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -3,6 +3,8 @@ import Card from '@mui/material/Card';
 
 export default function MyCard(props: {
   children: React.ReactNode;
+  raised?: boolean;
+  sx?: object;
 }): JSX.Element {
-  return <Card className={styles.card}>{props.children}</Card>;
+  return <Card {...props} className={styles.card} />;
 }
