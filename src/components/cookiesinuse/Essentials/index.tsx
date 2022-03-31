5b306b4b6456c02b337537feb776f83189c5c693
CarlosAugustoPO <eu.carlos.aug@gmail.com>
2022-03-31 00:17:47 -0300

    refactor: export EssentialCookie grid as component

--- src/components/cookiesinuse/Essentials/index.tsx
+++ src/components/cookiesinuse/Essentials/index.tsx
@@ -1,6 +1,6 @@
-import Caption from "src/components/Caption/index";
-import Grid from "@mui/material/Grid";
-import Divider from "@mui/material/Divider";
+import Caption from 'src/components/Caption/index';
+import Grid from '@mui/material/Grid';
+import Divider from '@mui/material/Divider';
 
 export default function EssentialsCookies() {
   return (
@@ -8,140 +8,154 @@ export default function EssentialsCookies() {
       container
       columnSpacing={2}
       style={{
-        padding: "2%",
-        minWidth: "200px",
-        textAlign: "left",
+        padding: '2%',
+        minWidth: '200px',
+        textAlign: 'left',
       }}
     >
       {/* {{{ Essentials 1*/}
       <Grid item xs={12}>
-        <Caption sx={{ fontWeight: 600 }}>Cookie essencial 1</Caption>{" "}
+        <Caption sx={{ fontWeight: 600 }}>
+          Cookie essencial 1
+        </Caption>{' '}
         <Divider />
       </Grid>
 
       <Grid item xs={4}>
-        <Caption sx={{ fontWeight: 600 }}>Nome:</Caption>{" "}
+        <Caption sx={{ fontWeight: 600 }}>Nome:</Caption>{' '}
       </Grid>
       <Grid item xs={8}>
-        <Caption>nextauth.message</Caption>{" "}
+        <Caption>nextauth.message</Caption>{' '}
       </Grid>
 
       <Grid item xs={4}>
-        <Caption sx={{ fontWeight: 600 }}>Fornecedor:</Caption>{" "}
+        <Caption sx={{ fontWeight: 600 }}>Fornecedor:</Caption>{' '}
       </Grid>
       <Grid item xs={8}>
-        <Caption>NextAuth.js</Caption>{" "}
+        <Caption>NextAuth.js</Caption>{' '}
       </Grid>
 
       <Grid item xs={4}>
-        <Caption sx={{ fontWeight: 600 }}>Funcão:</Caption>{" "}
+        <Caption sx={{ fontWeight: 600 }}>Função:</Caption>{' '}
       </Grid>
       <Grid item xs={8}>
-        <Caption>Comunica as ações do usuário entre as páginas</Caption>{" "}
+        <Caption>
+          Comunica as ações do usuário entre as páginas
+        </Caption>{' '}
       </Grid>
       <Grid item xs={4}>
-        <Caption sx={{ fontWeight: 600 }}>Tipo:</Caption>{" "}
+        <Caption sx={{ fontWeight: 600 }}>Tipo:</Caption>{' '}
       </Grid>
       <Grid item xs={8}>
-        <Caption>Cookie</Caption>{" "}
+        <Caption>Cookie</Caption>{' '}
       </Grid>
       {/*}}}*/}
       {/* {{{ Essentials 2*/}
       <Grid item xs={12}>
         <Divider />
-        <Caption sx={{ fontWeight: 600 }}>Cookie essencial 2</Caption>{" "}
+        <Caption sx={{ fontWeight: 600 }}>
+          Cookie essencial 2
+        </Caption>{' '}
         <Divider />
       </Grid>
       <Grid item xs={4}>
-        <Caption sx={{ fontWeight: 600 }}>Nome:</Caption>{" "}
+        <Caption sx={{ fontWeight: 600 }}>Nome:</Caption>{' '}
       </Grid>
       <Grid item xs={8}>
-        <Caption>next-auth.callback.url</Caption>{" "}
+        <Caption>next-auth.callback.url</Caption>{' '}
       </Grid>
       <Grid item xs={4}>
-        <Caption sx={{ fontWeight: 600 }}>Fornecedor:</Caption>{" "}
+        <Caption sx={{ fontWeight: 600 }}>Fornecedor:</Caption>{' '}
       </Grid>
       <Grid item xs={8}>
-        <Caption>NextAuth.js</Caption>{" "}
+        <Caption>NextAuth.js</Caption>{' '}
       </Grid>
       <Grid item xs={4}>
-        <Caption sx={{ fontWeight: 600 }}>Funcão:</Caption>{" "}
+        <Caption sx={{ fontWeight: 600 }}>Função:</Caption>{' '}
       </Grid>
       <Grid item xs={8}>
         <Caption>
-          Informa padrões de redirecionamento para o serviço de autenticação
-        </Caption>{" "}
+          Informa padrões de redirecionamento para o serviço de
+          autenticação
+        </Caption>{' '}
       </Grid>
       <Grid item xs={4}>
-        <Caption sx={{ fontWeight: 600 }}>Tipo:</Caption>{" "}
+        <Caption sx={{ fontWeight: 600 }}>Tipo:</Caption>{' '}
       </Grid>
       <Grid item xs={8}>
-        <Caption>Cookie</Caption>{" "}
+        <Caption>Cookie</Caption>{' '}
       </Grid>
       {/*}}}*/}
       {/* {{{ Essentials 3*/}
       <Grid item xs={12}>
         <Divider />
-        <Caption sx={{ fontWeight: 600 }}>Cookie essencial 3</Caption>{" "}
+        <Caption sx={{ fontWeight: 600 }}>
+          Cookie essencial 3
+        </Caption>{' '}
         <Divider />
       </Grid>
       <Grid item xs={4}>
-        <Caption sx={{ fontWeight: 600 }}>Nome:</Caption>{" "}
+        <Caption sx={{ fontWeight: 600 }}>Nome:</Caption>{' '}
       </Grid>
       <Grid item xs={8}>
-        <Caption>next-auth.csrf-token</Caption>{" "}
+        <Caption>next-auth.csrf-token</Caption>{' '}
       </Grid>
       <Grid item xs={4}>
-        <Caption sx={{ fontWeight: 600 }}>Fornecedor:</Caption>{" "}
+        <Caption sx={{ fontWeight: 600 }}>Fornecedor:</Caption>{' '}
       </Grid>
       <Grid item xs={8}>
-        <Caption>NextAuth.js</Caption>{" "}
+        <Caption>NextAuth.js</Caption>{' '}
       </Grid>
       <Grid item xs={4}>
-        <Caption sx={{ fontWeight: 600 }}>Funcão:</Caption>{" "}
+        <Caption sx={{ fontWeight: 600 }}>Função:</Caption>{' '}
       </Grid>
       <Grid item xs={8}>
-        <Caption>Garante a autenticidade do usuário ao fazer o login</Caption>{" "}
+        <Caption>
+          Garante a autenticidade do usuário ao fazer o login
+        </Caption>{' '}
       </Grid>
       <Grid item xs={4}>
-        <Caption sx={{ fontWeight: 600 }}>Tipo:</Caption>{" "}
+        <Caption sx={{ fontWeight: 600 }}>Tipo:</Caption>{' '}
       </Grid>
       <Grid item xs={8}>
-        <Caption>Cookie</Caption>{" "}
+        <Caption>Cookie</Caption>{' '}
       </Grid>
 
       {/*}}}*/}
       {/* {{{ Essentials 4*/}
       <Grid item xs={12}>
         <Divider />
-        <Caption sx={{ fontWeight: 600 }}>Cookie essencial 4</Caption>{" "}
+        <Caption sx={{ fontWeight: 600 }}>
+          Cookie essencial 4
+        </Caption>{' '}
         <Divider />
       </Grid>
       <Grid item xs={4}>
-        <Caption sx={{ fontWeight: 600 }}>Nome:</Caption>{" "}
+        <Caption sx={{ fontWeight: 600 }}>Nome:</Caption>{' '}
       </Grid>
       <Grid item xs={8}>
-        <Caption>ally-supports-cache</Caption>{" "}
+        <Caption>ally-supports-cache</Caption>{' '}
       </Grid>
       <Grid item xs={4}>
-        <Caption sx={{ fontWeight: 600 }}>Fornecedor:</Caption>{" "}
+        <Caption sx={{ fontWeight: 600 }}>Fornecedor:</Caption>{' '}
       </Grid>
       <Grid item xs={8}>
-        <Caption>ally.js</Caption>{" "}
+        <Caption>ally.js</Caption>{' '}
       </Grid>
       <Grid item xs={4}>
-        <Caption sx={{ fontWeight: 600 }}>Funcão:</Caption>{" "}
+        <Caption sx={{ fontWeight: 600 }}>Função:</Caption>{' '}
       </Grid>
       <Grid item xs={8}>
         <Caption>
-          Garante a funcionalidade de recursos de acessibilidade do navegador
-        </Caption>{" "}
+          Garante a funcionalidade de recursos de acessibilidade
+          do navegador
+        </Caption>{' '}
       </Grid>
       <Grid item xs={4}>
-        <Caption sx={{ fontWeight: 600 }}>Tipo:</Caption>{" "}
+        <Caption sx={{ fontWeight: 600 }}>Tipo:</Caption>{' '}
       </Grid>
       <Grid item xs={8}>
-        <Caption>Local Storage</Caption>{" "}
+        <Caption>Local Storage</Caption>{' '}
       </Grid>
 
       {/*}}}*/}
