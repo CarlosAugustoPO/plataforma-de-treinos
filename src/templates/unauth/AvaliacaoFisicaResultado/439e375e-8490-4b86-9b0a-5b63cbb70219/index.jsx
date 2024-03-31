91a03907d9a96fda697556e99b612ad84939dccb
CarlosAugustoPO <eu.carlos.aug@gmail.com>
2024-03-31 20:30:22 -0300

    feat: add af clara

---
+++ src/templates/unauth/AvaliacaoFisicaResultado/439e375e-8490-4b86-9b0a-5b63cbb70219/index.jsx
@@ -0,0 +1,480 @@
+import React from 'react';
+import GraficoComposicaoCorporal from 'src/components/GraficoComposicaoCorporal';
+import GraficoPercentualDeGordura from 'src/components/GraficoPercentualDeGordura';
+import GraficoDobrasCutaneas from 'src/components/GraficoDobrasCutaneas';
+import GraficoCircunferenciasDeTronco from 'src/components/GraficoCircunferenciasDeTronco';
+import GraficoCircunferenciasDeBraco from 'src/components/GraficoCircunferenciasDeBraco';
+import GraficoCircunferenciasDeMMII from 'src/components/GraficoCircunferenciasDeMMII';
+import GraficoPercentualDeMassaMuscular from 'src/components/GraficoPercentualDeMassaMuscular';
+import DobrasCutaneas from 'src/components/DobrasCutaneas';
+import TabelaResumoAF from 'src/components/TabelaResumoAF';
+import InformacoesPessoais from 'src/components/InformacoesPessoais';
+import InformacoesAdicionais from 'src/components/InformacoesAdicionais';
+import InformacoesNutricionais from 'src/components/InformacoesNutricionais';
+import InformacoesSobreOTreino from 'src/components/InformacoesSobreOTreino';
+import InformacoesPreAvaliacao from 'src/components/InformacoesPreAvaliacao';
+import ObjetivosExpectativas from 'src/components/ObjetivosExpectativas';
+import InformacoesSobreARotina from 'src/components/InformacoesSobreARotina';
+import InformacoesSobreASaude from 'src/components/InformacoesSobreASaude';
+import HistoricoDePeso from 'src/components/HistoricoDePeso';
+import ResumoAntropometrico from 'src/components/ResumoAntropometrico';
+import Circunferencias from 'src/components/Circunferencias';
+import AnaliseMorfologica from 'src/components/AnaliseMorfologica';
+import RecordatorioAlimentar from 'src/components/RecordatorioAlimentar';
+import AnalisePostural from 'src/components/AnalisePostural';
+import Title from 'src/components/Title/index';
+import Box from '@mui/material/Box';
+
+function ResultadoAvaliacaoFisica() {
+  const dadosDaAvaliacaoAtual = {
+    nome: 'Clara Conti Araújo',
+    idade: 49,
+    altura: 1.64,
+    email: 'claraconti@gmail.com',
+    dataDaAvaliação: '24/03/2024',
+    dataDeNascimento: '02/07/1974',
+    genero: 'feminino',
+    objetivoEstetico: 'Definição',
+    pontosFracosEsteticos: 'Abdômen e quadril',
+    pretendeCorrer: 'Não',
+    objetivosComCorrida: '',
+    pretendeMelhorarFlexibilidade: '',
+    objetivosComFlexibilidade: 'hide',
+    comoPossoTeAjudar: 'hide',
+    haQuantoTempoTreinaMusculacao: '10 anos',
+    motivoDeParadaDaUltimaVez: 'Gestação',
+    tempoDeParadaDaUltimaVez: '15 anos',
+    quantidadeDeTreinosPorSemana: '4 vezes',
+    quantidadeDeTreinosPorSemanaPretendido: '3 vezes',
+    horarioDeTreinoMusculacao: '22h00',
+    horarioDesejadoDeTreinoMusculacao: '11h00',
+    musculosPreferidosDeTreinoMusculacao: 'Sem preferências',
+    exerciciosComDificuldades: 'Exercícios de ombro, crucifixo',
+    tempoExerciciosAerobios: '',
+    horarioExerciciosAerobios: '',
+    horarioPretendidoExerciciosAerobios: '',
+    freqExerciciosAerobios: '',
+    tempoExerciciosAlongamento: '',
+    horarioExerciciosAlongamento: '',
+    horarioPretendidoExerciciosAlongamento: '',
+    ultimoTreino: '8 horas atrás',
+    ultimoTreinoDescricao: 'Quadríceps e glúteo',
+    ultimaRefeicao: 'Pão com ovos, queijo e café com leite',
+    tempoUltimaRefeicao: '90 minutos atrás',
+    horaDeDormir: '01:30',
+    horaDeAcordar: '06:30',
+    qualidadeDoSono: 'Boa',
+    rotinaDiariaDetalhes: '',
+    humorDiario: 'ansioso',
+    estrategiasContraEstresse: 'comer doces',
+    estrategiasDeLazer: 'Bater perna',
+    rotinaDeTrabalho: '',
+    meioDeTransporteParaTrabalho: '',
+    problemasDeSaude: 'Sem problemas de saúde diagnosticados',
+    tomaMedicamento: 'Sem medicamentos',
+    usaEsteroides: 'Não',
+    frequenciaAlcool: 'Não bebe',
+    frequenciaFumo: 'Não fuma',
+    lesaoPassada:
+      'Joelho falta de cartilagem, ombro com bursite e tendinite, cotovelo epicondilite',
+    limitacaoFisica: 'Lombar, joelho, ombro e cotovelo',
+    amamentando: '',
+    cirurgiaPassada: 'sem cirurgias',
+    tempoCirurgiaPassada: '',
+    fcRepouso: 'hide',
+    paRepouso: 'hide',
+    spO2: '',
+    temperaturaExtremidades: '',
+    temperaturaCorporal: '',
+    menorPesoAdulto: 52,
+    maiorPesoAdulto: 65,
+    peso3Meses: 60,
+    peso6Meses: '',
+    peso1Ano: 60,
+    peso5Anos: '',
+    peso10Anos: '',
+    pesoPretendido: 64,
+    pesoAtual: 52.9,
+    porcentagemDeGorduraBia: 27.1,
+    massaMuscularBia: 31.4,
+    idadeCorporal: 38,
+    gorduraVisceral: 5,
+    tmb: 1316,
+    circunferenciaDePunho: 15,
+    circunferenciaDeAbdomen: 78,
+    circunferenciaDePescoco: 32.5,
+    circunferenciaDeCintura: 71,
+    circunferenciaDeQuadril: 93,
+    circunferenciaDePanturrilha: 34.5,
+    circunferenciaBracoRelaxado: 27,
+    circunferenciaBracoContraido: 30,
+    circunferenciaTorax: 'hide',
+    circunferenciaAntebraco: 23,
+    circunferenciaCoxa: 55,
+    circunferenciaTornozelo: 'hide',
+    dobraCutaneaAxilarMedia: 10,
+    dobraCutaneaPeitoral: 9,
+    dobraCutaneaAbdominal: 20,
+    dobraCutaneaTricipital: 13,
+    dobraCutaneaSubescapular: 26,
+    dobraCutaneaSuprailiaca: 14,
+    dobraCutaneaCoxa: 17,
+    dobraCutaneaPanturrilha: 11,
+    planejaRefeicoes: 'Sim',
+    preparaRefeicoes: 'Sim',
+    numeroRefeicoes: '4 - 5 vezes por semana',
+    quantidadeAgua: '2l no mínimo',
+    horarioFome: 'a tarde',
+    alimentoBeliscar: 'Chocolate, bolacha, pão',
+    alimentosFrequentes:
+      'Carne, frango, ovos, Arroz, Pão de forma',
+    restricaoAlimentar: 'Sem restrições',
+    velocidadeMastigar: '',
+    usaSuplementos: 'Não',
+    consumoAlimentarDiario:
+      'Café: [Pão com ovo, café com leite, 1 fruta], Colação: [1 banana ou bolacha ou pão com mnteig], Almoço: [carne moída, batat doce, arroz, feijão e salada], Lanche da tarde: [Pão com manteiga], Lanche da tarde 2: [Fruta ou pão com queijo], Lanche da noite: [pão com carne moída e salada de fruta], Jantar = [pão com queijo]',
+    habitosFinaisDeSemana: 'Geralmente janta fora da rotina',
+    desvioPostural: 'Não possui',
+    informacoesAdicionais:
+      'Perna mais grossa, bumbum mais empinado',
+  };
+  const dadosDaAvaliacaoAnterior = {};
+
+  const peitoralAnterior =
+    dadosDaAvaliacaoAnterior.dobraCutaneaPeitoral;
+  const abdomenAnterior =
+    dadosDaAvaliacaoAnterior.dobraCutaneaAbdominal;
+  const tricepsAnterior =
+    dadosDaAvaliacaoAnterior.dobraCutaneaTricipital;
+  const subescapularAnterior =
+    dadosDaAvaliacaoAnterior.dobraCutaneaSubescapular;
+  const suprailiacaAnterior =
+    dadosDaAvaliacaoAnterior.dobraCutaneaSuprailiaca;
+  const coxaAnterior = dadosDaAvaliacaoAnterior.dobraCutaneaCoxa;
+  const panturrilhaAnterior =
+    dadosDaAvaliacaoAnterior.dobraCutaneaPanturrilha;
+  const axilarMediaAnterior =
+    dadosDaAvaliacaoAnterior.dobraCutaneaAxilarMedia;
+
+  // Cálculo de Gordura em % Pollock 7 Dobras
+  const somaDobrasAnterior =
+    peitoralAnterior +
+    abdomenAnterior +
+    tricepsAnterior +
+    subescapularAnterior +
+    suprailiacaAnterior +
+    coxaAnterior +
+    (axilarMediaAnterior
+      ? axilarMediaAnterior
+      : panturrilhaAnterior);
+
+  const calcularDensidadeCorporalAnterior = (
+    dadosDaAvaliacaoAnterior,
+  ) => {
+    var genero = dadosDaAvaliacaoAnterior.genero;
+
+    if (genero === 'masculino') {
+      const densidadeCorporalAnterior =
+        1.112 -
+        0.00043499 * somaDobrasAnterior +
+        0.00000055 * Math.pow(somaDobrasAnterior, 2) -
+        0.0002882 * dadosDaAvaliacaoAnterior.idade;
+      return densidadeCorporalAnterior;
+    } else if (genero === 'feminino') {
+      const densidadeCorporalAnterior =
+        1.097 -
+        0.00046971 * somaDobrasAnterior +
+        0.00000056 * Math.pow(somaDobrasAnterior, 2) -
+        0.00012828 * dadosDaAvaliacaoAnterior.idade;
+      return densidadeCorporalAnterior;
+    } else {
+      const densidadeCorporalAnterior =
+        1.097 -
+        0.00046971 * somaDobrasAnterior +
+        0.00000056 * Math.pow(somaDobrasAnterior, 2) -
+        0.00012828 * dadosDaAvaliacaoAnterior.idade;
+      return densidadeCorporalAnterior;
+    }
+  };
+
+  const pollock7dobrasAnterior =
+    (4.95 /
+      calcularDensidadeCorporalAnterior(
+        dadosDaAvaliacaoAnterior,
+      ) -
+      4.5) *
+    100;
+  // Cálculo de Gordura em KG Pollock 7 Dobras
+  const pesoGordoAnterior =
+    (pollock7dobrasAnterior / 100) *
+    dadosDaAvaliacaoAnterior.pesoAtual;
+  // Cálculo de Massa Magra em KG Pollock 7 Dobras
+  const pesoMagroAnterior =
+    dadosDaAvaliacaoAnterior.pesoAtual - pesoGordoAnterior;
+
+  const calcularIMCAnterior = (alturaAnterior, pesoAnterior) => {
+    return (
+      pesoAnterior /
+      (alturaAnterior * alturaAnterior)
+    ).toFixed(1);
+  };
+
+  const imcAnterior = calcularIMCAnterior(
+    dadosDaAvaliacaoAnterior.altura,
+    dadosDaAvaliacaoAnterior.pesoAtual,
+  );
+
+  const rcqAnterior = (
+    dadosDaAvaliacaoAnterior.circunferenciaDeCintura /
+    dadosDaAvaliacaoAnterior.circunferenciaDeQuadril
+  ).toFixed(2);
+
+  const peitoralAtual =
+    dadosDaAvaliacaoAtual.dobraCutaneaPeitoral;
+  const abdomenAtual =
+    dadosDaAvaliacaoAtual.dobraCutaneaAbdominal;
+  const tricepsAtual =
+    dadosDaAvaliacaoAtual.dobraCutaneaTricipital;
+  const subescapularAtual =
+    dadosDaAvaliacaoAtual.dobraCutaneaSubescapular;
+  const suprailiacaAtual =
+    dadosDaAvaliacaoAtual.dobraCutaneaSuprailiaca;
+  const coxaAtual = dadosDaAvaliacaoAtual.dobraCutaneaCoxa;
+  const panturrilhaAtual =
+    dadosDaAvaliacaoAtual.dobraCutaneaPanturrilha;
+  const axilarMediaAtual =
+    dadosDaAvaliacaoAtual.dobraCutaneaAxilarMedia;
+
+  // Cálculo de Gordura em % Pollock 7 Dobras
+  const somaDobrasAtual =
+    peitoralAtual +
+    abdomenAtual +
+    tricepsAtual +
+    subescapularAtual +
+    suprailiacaAtual +
+    coxaAtual +
+    (axilarMediaAtual ? axilarMediaAtual : panturrilhaAtual);
+
+  const calcularDensidadeCorporalAtual = (
+    dadosDaAvaliacaoAtual,
+  ) => {
+    var genero = dadosDaAvaliacaoAtual.genero;
+
+    if (genero === 'masculino') {
+      const densidadeCorporalAtual =
+        1.112 -
+        0.00043499 * somaDobrasAtual +
+        0.00000055 * Math.pow(somaDobrasAtual, 2) -
+        0.0002882 * dadosDaAvaliacaoAtual.idade;
+      return densidadeCorporalAtual;
+    } else if (genero === 'feminino') {
+      const densidadeCorporalAtual =
+        1.097 -
+        0.00046971 * somaDobrasAtual +
+        0.00000056 * Math.pow(somaDobrasAtual, 2) -
+        0.00012828 * dadosDaAvaliacaoAtual.idade;
+      return densidadeCorporalAtual;
+    } else {
+      const densidadeCorporalAtual =
+        1.097 -
+        0.00046971 * somaDobrasAtual +
+        0.00000056 * Math.pow(somaDobrasAtual, 2) -
+        0.00012828 * dadosDaAvaliacaoAtual.idade;
+      return densidadeCorporalAtual;
+    }
+  };
+
+  const pollock7dobrasAtual =
+    (4.95 /
+      calcularDensidadeCorporalAtual(dadosDaAvaliacaoAtual) -
+      4.5) *
+    100;
+  // Cálculo de Gordura em KG Pollock 7 Dobras
+  const pesoGordoAtual =
+    (pollock7dobrasAtual / 100) *
+    dadosDaAvaliacaoAtual.pesoAtual;
+  // Cálculo de Massa Magra em KG Pollock 7 Dobras
+  const pesoMagroAtual =
+    dadosDaAvaliacaoAtual.pesoAtual - pesoGordoAtual;
+
+  const calcularIMCAtual = (alturaAtual, pesoAtual) => {
+    return (pesoAtual / (alturaAtual * alturaAtual)).toFixed(1);
+  };
+
+  const imcAtual = calcularIMCAtual(
+    dadosDaAvaliacaoAtual.altura,
+    dadosDaAvaliacaoAtual.pesoAtual,
+  );
+
+  const rcqAtual = (
+    dadosDaAvaliacaoAtual.circunferenciaDeCintura /
+    dadosDaAvaliacaoAtual.circunferenciaDeQuadril
+  ).toFixed(2);
+
+  const dadosDoGrafico = [
+    {
+      data: dadosDaAvaliacaoAtual.dataDaAvaliação,
+      peso: dadosDaAvaliacaoAtual.pesoAtual,
+      idade: dadosDaAvaliacaoAtual.idade,
+      imc: imcAtual,
+      pollock7dobras: pollock7dobrasAtual.toFixed(1),
+      pesoMagro: pesoMagroAtual.toFixed(1),
+      pesoGordo: pesoGordoAtual.toFixed(1),
+      massaMuscularBia: dadosDaAvaliacaoAtual.massaMuscularBia,
+      porcentagemDeGorduraBia:
+        dadosDaAvaliacaoAtual.porcentagemDeGorduraBia,
+      gorduraVisceral: dadosDaAvaliacaoAtual.gorduraVisceral,
+      idadeCorporal: dadosDaAvaliacaoAtual.idadeCorporal,
+      circunferenciaDePunho:
+        dadosDaAvaliacaoAtual.circunferenciaDePunho,
+      circunferenciaDeAbdomen:
+        dadosDaAvaliacaoAtual.circunferenciaDeAbdomen,
+      circunferenciaDePescoco:
+        dadosDaAvaliacaoAtual.circunferenciaDePescoco,
+      circunferenciaDeCintura:
+        dadosDaAvaliacaoAtual.circunferenciaDeCintura,
+      circunferenciaDeQuadril:
+        dadosDaAvaliacaoAtual.circunferenciaDeQuadril,
+      circunferenciaDePanturrilha:
+        dadosDaAvaliacaoAtual.circunferenciaDePanturrilha,
+      circunferenciaBracoRelaxado:
+        dadosDaAvaliacaoAtual.circunferenciaBracoRelaxado,
+      circunferenciaBracoContraido:
+        dadosDaAvaliacaoAtual.circunferenciaBracoContraido,
+      circunferenciaTorax:
+        dadosDaAvaliacaoAtual.circunferenciaTorax,
+      circunferenciaAntebraco:
+        dadosDaAvaliacaoAtual.circunferenciaAntebraco,
+      circunferenciaCoxa:
+        dadosDaAvaliacaoAtual.circunferenciaCoxa,
+      circunferenciaTornozelo:
+        dadosDaAvaliacaoAtual.circunferenciaTornozelo,
+      rcq: rcqAtual,
+      dobraCutaneaAxilarMedia:
+        dadosDaAvaliacaoAtual.dobraCutaneaAxilarMedia,
+      dobraCutaneaPeitoral:
+        dadosDaAvaliacaoAtual.dobraCutaneaPeitoral,
+      dobraCutaneaAbdominal:
+        dadosDaAvaliacaoAtual.dobraCutaneaAbdominal,
+      dobraCutaneaTricipital:
+        dadosDaAvaliacaoAtual.dobraCutaneaTricipital,
+      dobraCutaneaSubescapular:
+        dadosDaAvaliacaoAtual.dobraCutaneaSubescapular,
+      dobraCutaneaSuprailiaca:
+        dadosDaAvaliacaoAtual.dobraCutaneaSuprailiaca,
+      dobraCutaneaCoxa: dadosDaAvaliacaoAtual.dobraCutaneaCoxa,
+      dobraCutaneaPanturrilha:
+        dadosDaAvaliacaoAtual.dobraCutaneaPanturrilha,
+      fcRepouso: dadosDaAvaliacaoAtual.fcRepouso,
+      paRepouso: dadosDaAvaliacaoAtual.paRepouso,
+    },
+  ];
+  /*adicionar mais um objeto dentro do array com
+   * dadosDaUltimaAvaliacao e dadosDaPrimeiraAvaliacao*/
+  return (
+    <Box
+      className="container"
+      sx={{
+        marginLeft: 'auto',
+        marginRight: 'auto',
+        marginTop: '30px',
+        marginBottom: '0',
+        backgroundColor: 'background.default',
+        overflow: 'hidden',
+      }}
+    >
+      <Title>Resultado da Avaliação Física</Title>
+      <Title paragraph sx={{ color: 'success.main' }}>
+        Data: {dadosDaAvaliacaoAtual.dataDaAvaliação}
+      </Title>
+
+      <InformacoesPessoais
+        dadosDaAvaliacao={dadosDaAvaliacaoAtual}
+      />
+
+      <TabelaResumoAF dadosDoGrafico={dadosDoGrafico} />
+
+      <ObjetivosExpectativas
+        dadosDaAvaliacao={dadosDaAvaliacaoAtual}
+      />
+
+      <InformacoesSobreOTreino
+        dadosDaAvaliacao={dadosDaAvaliacaoAtual}
+      />
+
+      <InformacoesPreAvaliacao
+        dadosDaAvaliacao={dadosDaAvaliacaoAtual}
+      />
+
+      <InformacoesSobreARotina
+        dadosDaAvaliacao={dadosDaAvaliacaoAtual}
+      />
+
+      <InformacoesSobreASaude
+        dadosDaAvaliacao={dadosDaAvaliacaoAtual}
+      />
+
+      <HistoricoDePeso
+        dadosDaAvaliacao={dadosDaAvaliacaoAtual}
+      />
+
+      <ResumoAntropometrico
+        dadosDaAvaliacao={dadosDaAvaliacaoAtual}
+      />
+
+      <Circunferencias
+        dadosDaAvaliacao={dadosDaAvaliacaoAtual}
+      />
+
+      <DobrasCutaneas dadosDaAvaliacao={dadosDaAvaliacaoAtual} />
+
+      <AnaliseMorfologica
+        dadosDaAvaliacao={dadosDaAvaliacaoAtual}
+      />
+
+      <InformacoesNutricionais
+        dadosDaAvaliacao={dadosDaAvaliacaoAtual}
+      />
+
+      <RecordatorioAlimentar
+        dadosDaAvaliacao={dadosDaAvaliacaoAtual}
+      />
+
+      <AnalisePostural
+        dadosDaAvaliacao={dadosDaAvaliacaoAtual}
+      />
+
+      <InformacoesAdicionais
+        dadosDaAvaliacao={dadosDaAvaliacaoAtual}
+      />
+
+      <GraficoCircunferenciasDeTronco
+        dadosDoGrafico={dadosDoGrafico}
+      />
+
+      <GraficoCircunferenciasDeBraco
+        dadosDoGrafico={dadosDoGrafico}
+      />
+
+      <GraficoCircunferenciasDeMMII
+        dadosDoGrafico={dadosDoGrafico}
+      />
+
+      <GraficoDobrasCutaneas dadosDoGrafico={dadosDoGrafico} />
+
+      <GraficoPercentualDeGordura
+        dadosDoGrafico={dadosDoGrafico}
+      />
+
+      <GraficoPercentualDeMassaMuscular
+        dadosDoGrafico={dadosDoGrafico}
+      />
+
+      <GraficoComposicaoCorporal
+        dadosDoGrafico={dadosDoGrafico}
+      />
+    </Box>
+  );
+}
+
+export default ResultadoAvaliacaoFisica;
